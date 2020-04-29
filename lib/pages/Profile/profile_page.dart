941f6460b621989f97759ac8604c5557b0f690a3
Joao Piga <joaof.victor@hotmail.com>
2020-04-29 16:12:56 -0300

    add page profile

--- lib/pages/Profile/profile_page.dart
+++ lib/pages/Profile/profile_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -9,7 +12,251 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.green[800],
+              size: 40,
+            ),
+            onPressed: () => Modular.to.pop()),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              _buildHeader(),
+              Container(
+                margin: const EdgeInsets.all(16.0),
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(color: Colors.grey.shade200),
+                child: Text(
+                    "Mobile and backend developer working in the market. Bachelor student in computer science."),
+              ),
+              _buildTitle("Skills"),
+              SizedBox(height: 10.0),
+              _buildSkillRow("Flutter", 0.65),
+              SizedBox(height: 5.0),
+              _buildSkillRow("React", 0.7),
+              SizedBox(height: 5.0),
+              _buildSkillRow("Laravel", 0.4),
+              SizedBox(height: 5.0),
+              _buildSkillRow("MySql", 0.3),
+              SizedBox(height: 30.0),
+              _buildTitle("Experience"),
+              _buildExperienceRow(
+                  company: "Onclick",
+                  position: "PHP (Laravel / Zend) Developer",
+                  duration: "2018 - 2018"),
+              _buildExperienceRow(
+                  company: "Nestlé",
+                  position: "Analist TI",
+                  duration: "2018 - 2019"),
+              _buildExperienceRow(
+                  company: "Hubess OutSourcing",
+                  position: "Mobile Developer",
+                  duration: "2019 - Today"),
+              SizedBox(height: 20.0),
+              _buildTitle("Education"),
+              SizedBox(height: 5.0),
+              _buildExperienceRow(
+                  company: "Univem",
+                  position: "Computer Science",
+                  duration: "2018 - 2021"),
+              SizedBox(height: 20.0),
+              _buildTitle("Contact"),
+              SizedBox(height: 5.0),
+              Row(
+                children: <Widget>[
+                  SizedBox(width: 30.0),
+                  Icon(
+                    Icons.mail,
+                    color: Colors.black54,
+                  ),
+                  SizedBox(width: 10.0),
+                  Text(
+                    "joaof.victor@hotmail.com",
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10.0),
+              Row(
+                children: <Widget>[
+                  SizedBox(width: 30.0),
+                  Icon(
+                    Icons.phone,
+                    color: Colors.black54,
+                  ),
+                  SizedBox(width: 10.0),
+                  Text(
+                    "+55 (14) 99626 7746",
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ],
+              ),
+              _buildSocialsRow(),
+              SizedBox(height: 20.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row _buildSocialsRow() {
+    return Row(
+      children: <Widget>[
+        SizedBox(width: 20.0),
+        IconButton(
+          color: Colors.indigo,
+          icon: Icon(FontAwesomeIcons.instagram),
+          onPressed: () {
+            _launchURL("https://www.instagram.com/joaovictorpiga/");
+          },
+        ),
+        SizedBox(width: 5.0),
+        IconButton(
+          color: Colors.indigo,
+          icon: Icon(FontAwesomeIcons.github),
+          onPressed: () {
+            _launchURL("https://github.com/DevJoaoLopes");
+          },
+        ),
+        SizedBox(width: 5.0),
+        IconButton(
+          color: Colors.red,
+          icon: Icon(FontAwesomeIcons.gitlab),
+          onPressed: () {
+            _launchURL("https://gitlab.com/joaopiga");
+          },
+        ),
+        SizedBox(width: 5.0),
+        IconButton(
+          color: Colors.red,
+          icon: Icon(FontAwesomeIcons.linkedin),
+          onPressed: () {
+            _launchURL("https://www.linkedin.com/in/jo%C3%A3o-victor-faustino-piga-lopes-4b027216a/");
+          },
+        ),
+        SizedBox(width: 10.0),
+      ],
+    );
+  }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  ListTile _buildExperienceRow(
+      {String company, String position, String duration}) {
+    return ListTile(
+      leading: Padding(
+        padding: const EdgeInsets.only(top: 8.0, left: 20.0),
+        child: Icon(
+          FontAwesomeIcons.solidCircle,
+          size: 12.0,
+          color: Colors.black54,
+        ),
+      ),
+      title: Text(
+        company,
+        style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+      ),
+      subtitle: Text("$position ($duration)"),
+    );
+  }
+
+  Row _buildSkillRow(String skill, double level) {
+    return Row(
+      children: <Widget>[
+        SizedBox(width: 16.0),
+        Expanded(
+            flex: 2,
+            child: Text(
+              skill.toUpperCase(),
+              textAlign: TextAlign.right,
+            )),
+        SizedBox(width: 10.0),
+        Expanded(
+          flex: 5,
+          child: LinearProgressIndicator(
+            value: level,
+          ),
+        ),
+        SizedBox(width: 16.0),
+      ],
+    );
+  }
+
+  Widget _buildTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            title.toUpperCase(),
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          ),
+          Divider(
+            color: Colors.black54,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Row _buildHeader() {
+    return Row(
+      children: <Widget>[
+        SizedBox(width: 20.0),
+        Container(
+            width: 80.0,
+            height: 80.0,
+            child: CircleAvatar(
+                radius: 40,
+                backgroundColor: Colors.grey,
+                child: CircleAvatar(
+                    radius: 35.0,
+                    backgroundImage:
+                        AssetImage('lib/shared/assets/profile.png')))),
+        SizedBox(width: 20.0),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Joao Victor Piga",
+              style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10.0),
+            Text("Mobile Developer"),
+            SizedBox(height: 5.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  FontAwesomeIcons.map,
+                  size: 12.0,
+                  color: Colors.black54,
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  "São Paulo, Brasil",
+                  style: TextStyle(color: Colors.black54),
+                ),
+              ],
+            ),
+          ],
+        )
+      ],
     );
   }
 }
